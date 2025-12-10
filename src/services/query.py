d4095bee76504c0337a9dbb7126266db8d4e4b6a
vcnt <thiago.vicente@ua.pt>
2025-12-10 19:48:29 +0000

    window_start_time -> window_end_time

--- src/services/query.py
+++ src/services/query.py
@@ -18,7 +18,7 @@ class QueryCH:
     WHERE cell_index = {cell_index:Int32}
       AND toUnixTimestamp(window_start_time) >= {start_time:Int64}
       AND toUnixTimestamp(window_end_time) <= {end_time:Int64}
-    ORDER BY window_start_time DESC
+    ORDER BY window_end_time DESC
     LIMIT {limit:Int32}
     OFFSET {offset:Int32}
     """
